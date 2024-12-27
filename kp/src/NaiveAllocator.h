3a75b64d533db7c3ef1c86e69467af03f1f0dc77
Samemantlt <46570878+Samemantlt@users.noreply.github.com>
2024-12-27 22:09:59 +0300

    WIP kp done

--- kp/src/NaiveAllocator.h
+++ kp/src/NaiveAllocator.h
@@ -3,12 +3,15 @@
 #include <vector>
 #include <cstddef>
 #include <map>
+#include <unordered_set>
 #include <unordered_map>
 #include <list>
 #include <sys/mman.h>
 #include "AllocatorBase.h"
 
 const int alignment = alignof(std::max_align_t);
+
+const int MIN_SPLIT_NEW_BLOCK_SIZE = 100;
 
 class NaiveAllocator : public AllocatorBase {
 public:
@@ -31,10 +34,14 @@ public:
 
     void * alloc(size_t block_size) override {
         block_size = (block_size / alignment + 1) * alignment;
-        for (auto& block : blocks) {
-            if (block.is_free && block.size >= block_size) {
-                block.is_free = false;
-                return block.ptr;
+        for (size_t i = 0; i < blocks.size(); ++i) {
+            if (blocks[i].is_free && blocks[i].size >= block_size) {
+                if (blocks[i].size > block_size + MIN_SPLIT_NEW_BLOCK_SIZE) { // Если остаток достаточно большой для нового блока
+                    split_block(i, block_size);
+                }
+
+                blocks[i].is_free = false;
+                return blocks[i].ptr;
             }
         }
 
@@ -53,14 +60,42 @@ public:
     }
 
     void* free(void* block_ptr) override {
-        for (auto& block : blocks) {
-            if (block.ptr == block_ptr) {
-                block.is_free = true;
+        for (size_t i = 0; i < blocks.size(); ++i) {
+            if (blocks[i].ptr == block_ptr) {
+                blocks[i].is_free = true;
+                // Проверяем соседние блоки
+                merge_with_neighbors(i);
                 break;
             }
         }
 
         return block_ptr;
+    }
+
+private:
+    void split_block(size_t block_index, size_t block_size) {
+        // Создаем новый свободный блок из оставшейся части
+        Block new_block{
+            reinterpret_cast<char*>(blocks[block_index].ptr) + block_size,
+            blocks[block_index].size - block_size,
+            true
+        };
+        blocks[block_index].size = block_size;
+        blocks.insert(blocks.begin() + block_index + 1, new_block);
+    }
+
+    void merge_with_neighbors(size_t index) {
+        // Проверяем блок справа
+        if (index + 1 < blocks.size() && blocks[index + 1].is_free) {
+            blocks[index].size += blocks[index + 1].size; // Увеличиваем размер текущего блока
+            blocks.erase(blocks.begin() + (index + 1)); // Удаляем следующий блок
+        }
+        // Проверяем блок слева (если не первый)
+        if (index > 0 && blocks[index - 1].is_free) {
+            blocks[index - 1].size += blocks[index].size; // Увеличиваем размер блока слева
+            blocks.erase(blocks.begin() + index); // Удаляем текущий блок
+            // Нет необходимости уменьшать index, так как продолжаем с текущей позиции
+        }
     }
 };
 
